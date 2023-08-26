a7e62748c74c948ec522a32a40171065c1213796
Silas Leite <bigchill_silas@hotmail.com>
2023-08-26 10:39:01 -0300

    :sparkles: feat: Change project language from kotlin to java

---
+++ EducARApp/app/src/main/java/br/gov/sp/educarapp/MainActivity.java
@@ -0,0 +1,14 @@
+package br.gov.sp.educarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
